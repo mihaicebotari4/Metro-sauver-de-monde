583b499e7bdb2a690c0667582fa149e22364fac7
mihaicebotari4 <mihai.cebotari4@gmail.com>
2026-01-23 17:12:33 -0500

    push to computer

--- metro sauver de monde/Assets/script/player.cs
+++ metro sauver de monde/Assets/script/player.cs
@@ -10,9 +10,10 @@ public float rotationspeed;
    private Transform trplayer;
    private float mouvementx =0;
    private float mouvementz = 0;
+   private ConstantForce constantForce;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {constantForce = GetComponent<ConstantForce>();
         trplayer = GetComponent<Transform>();
     }
 
@@ -23,6 +24,8 @@ public float rotationspeed;
          transform.Rotate(0, rotation*rotationspeed*Time.deltaTime, 0);
          mouvementx = (float) Math.Sin(Math.PI/180*transform.rotation.eulerAngles.y);
          mouvementz = (float) Math.Cos(Math.PI/180*transform.rotation.eulerAngles.y);
-        trplayer.position = new Vector3(trplayer.position.x+(mouvementx*movespeed*Time.deltaTime), trplayer.position.y, trplayer.position.z+(mouvementz*movespeed*Time.deltaTime));
+    constantForce.relativeForce = new Vector3(0, movespeed*Time.deltaTime,0);
+    
+      //  trplayer.position = new Vector3(trplayer.position.x+(mouvementx*movespeed*Time.deltaTime), trplayer.position.y, trplayer.position.z+(mouvementz*movespeed*Time.deltaTime));
     }
 }
